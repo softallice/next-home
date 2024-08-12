846b82a191a2969cc382133d00629226337dc5fa
s991045 <s991045@gmail.com>
2024-08-12 16:26:03 +0900

    [개발] 메가메뉴

--- frontend/src/components/header/MegaMenu.tsx
+++ frontend/src/components/header/MegaMenu.tsx
@@ -1,10 +1,8 @@
 "use client"
 
 import * as React from "react"
-import Link from "next/link"
-
 import { cn } from "@/lib/utils"
-// import { Icons } from "@/components/icons"
+
 import {
   NavigationMenu,
   NavigationMenuContent,
